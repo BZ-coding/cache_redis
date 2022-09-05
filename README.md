3eabc9e0155293a239bd6d9242bc78a8dc425c88
BZ <435878393@qq.com>
2022-09-06 01:45:33 +0800

    first

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cache_redis
+
+使用redis作为数据库的缓存。当然，也可以单独使用redis。
---
+++ __init__.py
@@ -0,0 +1,23 @@
+import json
+
+import redis
+
+
+class CacheRedis:
+    def __init__(self, host, port, db=0):
+        self.cache_redis = redis.Redis(host=host, port=port, db=db)
+
+    def get(self, key):
+        return self.cache_redis.get(key)
+
+    def set(self, key, data):
+        self.cache_redis.set(key, json.dumps(data))
+
+    def get_cache_from_db(self, key: str, get_db_func):
+        data_redis = self.cache_redis.get(key)
+        if data_redis is None:
+            data = get_db_func()
+            self.cache_redis.set(key, data)
+        else:
+            data = json.loads(data_redis)
+        return data
