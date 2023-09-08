b40ed064ab7103b4222b01ba76194bc300ee06b5
BZ <435878393@qq.com>
2023-09-08 22:38:25 +0800

    增加ex过期时间

--- __init__.py
+++ __init__.py
@@ -13,12 +13,12 @@ class CacheRedis:
     def set(self, key, data):
         self.cache_redis.set(key, json.dumps(data))
 
-    def get_cache_from_db(self, key: str, get_db_func):
+    def get_cache_from_db(self, key: str, get_db_func, ex=None):
         data_redis = self.cache_redis.get(key)
         if data_redis is None:
             data = get_db_func()
             data_str = json.dumps(data)
-            self.cache_redis.set(key, data_str)
+            self.cache_redis.set(key, data_str, ex=ex)
         else:
             data = json.loads(data_redis)
         return data
